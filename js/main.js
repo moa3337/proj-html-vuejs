45dcd27ac6e2a102093da37fd47a1652d55f0d93
moa3337 <moa-x@hotmail.com>
2023-02-21 20:53:37 +0100

    completamento navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 const { createApp } = Vue;
-console.log("ciao");
+
 const app = createApp({
     data() {
         return {
-        }
-    }
+            menu: [
+                'HOME',
+                'PAGES',
+                'PORTFOLIO',
+                'BLOG',
+                'SHOP',
+                'ELEMENTS'
+            ],
+            showList: false,
+        };
+    },
+    methods: {
+        toggleLIst() {
+            this.showList = !this.showList;
+        },
+    },
 });
 
 app.mount("#app");
+
+/*
+{
+    home: 'HOME',
+    page: 'PAGES',
+    portf: 'PORTFOLIO',
+    blog: 'BLOG',
+    shop: 'SHOP',
+    page: 'PAGES'
+},
+*/
